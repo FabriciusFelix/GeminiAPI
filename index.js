cc1ffeae9868dc42e945212e7912b81e144afbc0
FabriciusFelix <fabricius.felix@yahoo.com.br>
2024-03-31 18:24:43 -0300

    Atualizações Back-End para o Front

--- index.js
+++ index.js
@@ -1,42 +1,107 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const dotenv = require("dotenv");
+var cors = require("cors");
+dotenv.config()
+
 const express = require('express');
 const path = require('path');
-const app = express();
+var app = express();
+app.use(cors(corsOptions));
+
+var corsOptions = {
+    origin: 'http://localhost:5500',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
 
 const router = express.Router();    
-router.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname + '/pages/home.html'))
+router.get('/',(cors(corsOptions)), (req, res)=>{
+    res.sendFile(path.join(__dirname + '/pages/home.html'));
+    res.json({success: true})
+})
+router.get('/home',(cors(corsOptions)), (req, res)=>{
+    res.sendFile(path.join(__dirname + '/pages/home.html'));
+    
 })
 router.get('/contato', (req, res)=>{
+    
     res.sendFile(path.join(__dirname + '/pages/contato.html'))
 })
+
+
+router.get('/prompt',(cors(corsOptions)), (req, res)=>{
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY)
+    var responseToFront = '';            
+    console.log(req.url)
+    console.log('Body'+req.body)
+    console.log('Carregando genAI')
+    async function run() {
+       const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+       const prompt = req.url.replace('/prompt?','')
+       const result = await model.generateContent(prompt);
+       const response = await result.response;
+       const text = response.text();
+       responseToFront = text;
+       console.log( 'Response dentro da api: '+ responseToFront) 
+       res.json(text);
+    }
+    run();
+})
+
+app.get('/api/user',(req,res) => {
+    const user = [{
+        id:123,
+        name: 'John'
+    },{
+        id:234,
+        name: 'Bob'
+    },{
+        id:345,
+        name: 'Sue'
+    }];
+    res.json(user);
+});
+app.post('/prompt',function (req, res){
+    console.log('dentro do Post')
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY)
+    var responseToFront = '';    
+    let data = req.body;
+    console.log('Valor data: ',req.body)
+    if (tipeof(data) == undefined) {
+        data = 'Quantos anos Micheal Jackson tinha?'
+    }
+    console.log(req.body)
+    console.log('Carregando genAI')
+    async function run() {
+       const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+       const prompt = data;
+       const result = await model.generateContent(prompt);
+       const response = await result.response;
+       const text = response.text;
+       responseToFront = text;
+       console.log( 'Response dentro da api: '+ responseToFront) 
+       res.json(responseToFront);
+    }
+    run();
+})
+app.post('/api/prompt', (req, res)=>{
+    console.log('dentro do Post')
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY)   
+    let data = '';
+    console.log(req.body)
+    console.log('Carregando genAI')
+    async function run() {
+       const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+       const prompt = req.body;
+       const result = await model.generateContent(prompt);
+       const response = await result.response;
+       const text = response.text;
+       responseToFront = text;
+       console.log( 'Response dentro da api: '+ responseToFront) 
+       res.send(responseToFront)
+    }
+    run();        
+})
+
+
 app.use(router)
 app.listen(process.env.PORT || 7070, ()=> console.log('server listening on port 7070'));
-// process.title = 'GeminiApi';
-// var args = process.argv,
-//    port = args[2] || 7070,
-//    webServer = require('./server');
-
-//  webServer.listen(port, function() {
-//    console.log('Server started at port ' + port);
-//  });
-
-
-// const { GoogleGenerativeAI } = require("@google/generative-ai");
-// const dotenv = require("dotenv");
-// dotenv.config()
-// // Access your API key as an environment variable (see "Set up your API key" above)
-// const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-
-// async function run() {
-//   // For text-only input, use the gemini-pro model
-//   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-  
-//   const prompt = "O que é você?"
-
-//   const result = await model.generateContent(prompt);
-//   const response = await result.response;
-//   const text = response.text();
-//   console.log(text);
-// }
-
-// run();
---
+++ readme.md
@@ -0,0 +1,5 @@
+npm install express
+npm i dotenv @google/generative-ai
+npm install --save-dev nodemon 
+    ToExecute: npx nodemon index.js
+    
